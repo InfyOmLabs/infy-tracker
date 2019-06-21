e5cca16154f9a2f7264fce8fe23bb789c0d5e4d4
Monika vaghasiya <monika.vaghasiya@infyom.com>
2019-06-21 17:33:39 +0530

    feat(Task): Task detail css changes

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,6 +9,7 @@
           href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/min/dropzone.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" />
+    <link href="{{mix('assets/style/css/task-detail.css')}}" rel="stylesheet" type="text/css"/>
 @endsection
 @section('content')
     <div class="container-fluid">
@@ -29,16 +30,16 @@
                                 <h4 class="mb-3">{{$task->title}}</h4>
                             </div>
                         </div>
-                        <div class="row d-flex flex-wrap task-div">
-                            <div class="mb-3 d-flex w-40">
+                        <div class="row task-div d-flex">
+                            <div class="mb-3 d-flex task-detail__width">
                                 <span class="task-detail__heading font-weight-bold">Project:</span>
                                 <span class="flex-1">{{$task->project->name}}</span>
                             </div>
-                            <div class="mb-3 d-flex w-40">
+                            <div class="mb-3 d-flex task-detail__width">
                                 <span class="task-detail__heading font-weight-bold">Updated At:</span>
                                 <span class="flex-1">{{$task->updated_at->format('d F, Y h:i A')}}</span>
                             </div>
-                            <div class="mb-3 d-flex w-40">
+                            <div class="mb-3 d-flex task-detail__width">
                                 <span class="task-detail__heading font-weight-bold">Status:</span>
                                 <span class="flex-1">
                                     @if($task->status=='0')
@@ -48,28 +49,28 @@
                                     @endif
                                 </span>
                             </div>
-                            <div class="mb-3 d-flex w-40">
+                            <div class="mb-3 d-flex task-detail__width">
                                 <span class="task-detail__heading font-weight-bold">Created At:</span>
                                 <span class="flex-1">{{$task->created_at->format('d F, Y h:i A')}}</span>
                             </div>
-                            <div class="mb-3 d-flex w-40">
+                            <div class="mb-3 d-flex task-detail__width">
                                 <span class="task-detail__heading font-weight-bold">Reporter:</span>
                                 <span class="flex-1">{{$task->createdUser->name}}</span>
                             </div>
                             @if(!empty($task->due_date))
-                                <div class="mb-3 d-flex w-40">
+                                <div class="mb-3 d-flex task-detail__width">
                                     <span class="task-detail__heading font-weight-bold">Due date:</span>
                                     <span class="flex-1">{{$task->due_date}}</span>
                                 </div>
                             @endif
                             @if(!empty($task->taskAssignee->pluck('name')->toArray()))
-                                <div class="mb-3 d-flex w-40">
+                                <div class="mb-3 d-flex task-detail__width">
                                     <span class="task-detail__heading font-weight-bold">Assignee:</span>
                                     <span class="flex-1">{{implode(", ",$task->taskAssignee->pluck('name')->toArray())}}</span>
                                 </div>
                             @endif
                             @if(!empty($task->tags->pluck('name')->toArray()))
-                                <div class="mb-3 d-flex w-40">
+                                <div class="mb-3 d-flex task-detail__width">
                                     <span class="task-detail__heading font-weight-bold">Tags:</span>
                                     <span class="flex-1">{{implode(", ",$task->tags->pluck('name')->toArray())}}</span>
                                 </div>
@@ -78,7 +79,7 @@
                         <div class="row">
                             <div class="col-lg-8 col-sm-12">
                                 <div class="mb-3 d-flex">
-                                    <span class="dis-header font-weight-bold">Description:</span>
+                                    <span class="task-detail-lg-8__heading font-weight-bold">Description:</span>
                                     <span class="flex-1">{{$task->description}}</span>
                                 </div>
                             </div>
@@ -86,7 +87,7 @@
                         <div class="row">
                             <div class="col-lg-8 col-sm-12">
                                 <div class="mb-3 d-flex">
-                                    <span class="task-detail__heading font-weight-bold">Attachments</span>
+                                    <span class="font-weight-bold">Attachments</span>
                                 </div>
                             </div>
                         </div>
