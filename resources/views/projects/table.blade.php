3b07ca3a7ce1b271d130550e4474c39f551b5e94
Shailesh Ladumor <shailesh.infyom@gmail.com>
2019-06-22 16:12:35 +0530

    feat: prefix column added into project table

--- resources/views/projects/table.blade.php
+++ resources/views/projects/table.blade.php
@@ -1,6 +1,7 @@
 <table class="table table-responsive-sm table-striped table-bordered" id="projects_table">
     <thead>
     <tr>
+        <th>Prefix</th>
         <th>Name</th>
         <th>Client</th>
         <th>Action</th>
