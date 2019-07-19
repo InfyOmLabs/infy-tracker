bac460604f2833167f9c8ea1224fc2549adefc85
Shailesh Ladumor <shailesh.infyom@gmail.com>
2019-07-19 13:58:51 +0530

    fix: stlye ci issue

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -149,7 +149,7 @@ class TaskController extends AppBaseController
     /**
      * Update the specified Task in storage.
      *
-     * @param Task $task
+     * @param Task              $task
      * @param UpdateTaskRequest $request
      *
      * @throws Exception
@@ -238,7 +238,7 @@ class TaskController extends AppBaseController
     }
 
     /**
-     * @param Task $task
+     * @param Task    $task
      * @param Request $request
      *
      * @throws Exception
