6b045775067282dabbd88b9f126406580d522d0e
Harshad <harshad.prajapati@infyom.com>
2019-07-19 11:14:41 +0530

    fix(loader): replacement of loader

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -31,9 +31,16 @@ function customSearch() {
 }
 
 function getSpinner() {
-    return '<div class="spinner">\n' +
-        '  <div class="double-bounce1"></div>\n' +
-        '  <div class="double-bounce2"></div>\n' +
+    return '<div id="infyLoader" class="infy-loader">\n' +
+        '    <svg width="150px" height="75px" viewBox="0 0 187.3 93.7" preserveAspectRatio="xMidYMid meet"\n' +
+        '         style="left: 50%; top: 50%; position: absolute; transform: translate(-50%, -50%) matrix(1, 0, 0, 1, 0, 0);">\n' +
+        '        <path stroke="#00c6ff" id="outline" fill="none" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"\n' +
+        '              stroke-miterlimit="10"\n' +
+        '              d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 \t\t\t\tc-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z"/>\n' +
+        '        <path id="outline-bg" opacity="0.05" fill="none" stroke="#f5981c" stroke-width="5" stroke-linecap="round"\n' +
+        '              stroke-linejoin="round" stroke-miterlimit="10"\n' +
+        '              d="\t\t\t\tM93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 \t\t\t\tc-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z"/>\n' +
+        '    </svg>\n' +
         '</div>';
 }
 
