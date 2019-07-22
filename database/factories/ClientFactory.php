90d4da41792ff248f14eb0fabe541fd94979fc84
Vishal Ribdiya <33887755+vishalinfyom@users.noreply.github.com>
2019-07-22 14:17:21 +0530

    style: Apply fixes from StyleCI (#139)

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Models\Client;
--- tests/Controllers/Validations/ClientControllerValidationTest.php
+++ tests/Controllers/Validations/ClientControllerValidationTest.php
@@ -7,8 +7,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 /**
- * Class ClientControllerValidationTest
- * @package Tests\Controllers\Validations
+ * Class ClientControllerValidationTest.
  */
 class ClientControllerValidationTest extends TestCase
 {
