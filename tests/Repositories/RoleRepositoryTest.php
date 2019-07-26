99966982b6d69274e2d62fd16d3ef5fed2d2c001
Farhan <farhanali.kathawala@infyom.com>
2019-07-26 14:24:50 +0530

    test(roles): test cases for roles repo

---
+++ tests/Repositories/RoleRepositoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Repositories;
+
+use App\Models\Client;
+use App\Models\Role;
+use App\Repositories\RoleRepository;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+/**
+ * Class RoleRepositoryTest.
+ */
+class RoleRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var RoleRepository */
+    protected $roleRepo;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->roleRepo = app(RoleRepository::class);
+    }
+
+    /** @test */
+    public function it_can_retrieve_roles_list()
+    {
+        factory(Role::class)->times(3)->create();
+
+        $roles = $this->roleRepo->getRolesList();
+
+        // 1 default Admin role
+        $this->assertCount(4, $roles);
+
+        $roles->map(function ($client) use ($roles) {
+            $this->assertContains($client, $roles);
+        });
+    }
+}
