cb75a5df0c2b9d4b11151a778655a94c66cdc163
Farhan <farhanali.kathawala@infyom.com>
2019-07-27 18:06:28 +0530

    test(report): test cases for report controller validation API

---
+++ tests/Controllers/Validations/ReportControllerValidationTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Controllers\Validations;
+
+use App\Models\Report;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+/**
+ * Class ClientControllerValidationTest.
+ */
+class ReportControllerValidationTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->signInWithDefaultAdminUser();
+    }
+
+    /** @test */
+    public function test_create_report_fails_when_name_is_not_passed()
+    {
+        $this->post('reports', ['name' => ''])
+            ->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function test_create_report_fails_when_start_date_is_not_passed()
+    {
+        $input = [
+            'name'       => 'random string',
+            'start_date' => '',
+        ];
+
+        $this->post('reports', $input)->assertSessionHasErrors([
+            'start_date' => 'The start_date field is required.',
+        ]);
+    }
+
+    /** @test */
+    public function test_create_report_fails_when_end_date_is_not_passed()
+    {
+        $input = [
+            'name'       => 'random string',
+            'start_date' => '2019-07-19 03:53:07',
+            'end_date'   => '',
+        ];
+
+        $this->post('reports', $input)->assertSessionHasErrors([
+            'end_date' => 'The end_date field is required.',
+        ]);
+    }
+
+    /** @test */
+    public function test_update_report_fails_when_name_is_not_passed()
+    {
+        $report = factory(Report::class)->create();
+
+        $this->put('reports/'.$report->id, ['name' => ''])
+            ->assertSessionHasErrors(['name' => 'The name field is required.']);
+    }
+
+    /** @test */
+    public function test_update_report_fails_when_start_date_is_not_passed()
+    {
+        $report = factory(Report::class)->create();
+        $input = [
+            'name'       => 'random string',
+            'start_date' => '',
+        ];
+
+        $this->put('reports/'.$report->id, $input)
+            ->assertSessionHasErrors(['name' => 'The start_date field is required.']);
+    }
+
+    /** @test */
+    public function test_update_report_fails_when_end_date_is_not_passed()
+    {
+        $report = factory(Report::class)->create();
+        $input = [
+            'name'       => 'random string',
+            'start_date' => '2019-07-19 03:53:07',
+            'end_date'   => '',
+        ];
+
+        $this->put('reports/'.$report->id, $input)
+            ->assertSessionHasErrors(['name' => 'The end_date field is required.']);
+    }
+}
