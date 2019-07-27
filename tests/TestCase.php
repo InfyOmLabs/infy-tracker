1d0b21aa12ca9da20a2c37c3872ebf47e8e9cef4
Mitul Golakiya <mtl.golakiya@gmail.com>
2019-07-27 12:47:50 +0530

    test(controllers): controllers test cases added (#153)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Tests;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -15,6 +16,25 @@ abstract class TestCase extends BaseTestCase
         $user = User::first();
 
         return $this->actingAs($user);
+    }
+
+    public function assertSuccessMessageResponse(TestResponse $response, string $message)
+    {
+        $response->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'message' => $message,
+            ]);
+    }
+
+    public function assertSuccessDataResponse(TestResponse $response, array $data, string $message)
+    {
+        $response->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'message' => $message,
+                'data'    => $data,
+            ]);
     }
 
     /**
