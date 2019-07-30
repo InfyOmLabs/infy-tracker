a9510f7210b7353b9934a32b95a3f8005f49ab9f
vishalinfyom <vishal.ribdiya@infyom.com>
2019-07-30 19:05:46 +0530

    test(roles): removed tests of views/redirect methods

--- tests/Controllers/RoleControllerTest.php
+++ tests/Controllers/RoleControllerTest.php
@@ -4,63 +4,17 @@ namespace Tests\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
-use App\Repositories\RoleRepository;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Mockery\MockInterface;
 use Tests\TestCase;
 
 class RoleControllerTest extends TestCase
 {
     use DatabaseTransactions;
 
-    /** @var MockInterface */
-    protected $roleRepository;
-
     public function setUp(): void
     {
         parent::setUp();
         $this->signInWithDefaultAdminUser();
-        $this->withHeaders(['X-Requested-With' => 'XMLHttpRequest']);
-    }
-
-    private function mockRepository()
-    {
-        $this->roleRepository = \Mockery::mock(RoleRepository::class);
-        app()->instance(RoleRepository::class, $this->roleRepository);
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-        \Mockery::close();
-    }
-
-    /** @test */
-    public function it_can_store_role()
-    {
-        $this->mockRepository();
-
-        /** @var Role $role */
-        $role = factory(Role::class)->make()->toArray();
-
-        $this->roleRepository->shouldReceive('create')
-            ->once()
-            ->with($role)
-            ->andReturn([]);
-
-        $response = $this->postJson('roles', $role);
-        $response->assertStatus(302);
-    }
-
-    /** @test */
-    public function it_can_retrieve_role()
-    {
-        /** @var Role $role */
-        $role = factory(Role::class)->create();
-
-        $response = $this->getJson('roles/'.$role->id.'/edit');
-
-        $response->assertStatus(200);
     }
 
     /** @test */
