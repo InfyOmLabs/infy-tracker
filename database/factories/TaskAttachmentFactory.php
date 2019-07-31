d37bccb72d1845ce3d763e793b86fd5ebfdfdcf4
Farhan <farhanali.kathawala@infyom.com>
2019-07-31 16:13:16 +0530

    test(task): remove comment

--- database/factories/TaskAttachmentFactory.php
+++ database/factories/TaskAttachmentFactory.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Company: InfyOm Technologies, Copyright 2019, All Rights Reserved.
- * Author: Vishal Ribdiya
- * Email: vishal.ribdiya@infyom.com
- * Date: 27-07-2019
- * Time: 05:21 PM.
- */
-
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Models\Task;
