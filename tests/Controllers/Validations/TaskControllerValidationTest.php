054d72a6c922bacedc311e8a06c8f48b5cd03bf1
Farhan <farhanali.kathawala@infyom.com>
2019-08-02 16:36:06 +0530

    test(task): test cases for task controller API

--- tests/Controllers/Validations/TaskControllerValidationTest.php
+++ tests/Controllers/Validations/TaskControllerValidationTest.php
@@ -33,6 +33,25 @@ class TaskControllerValidationTest extends TestCase
     }
 
     /** @test */
+    public function test_create_task_fail_with_non_existing_project_id()
+    {
+        $this->post('tasks', ['project_id' => 999])
+            ->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function test_create_task_fail_with_invalid_due_date()
+    {
+        $task = factory(Task::class)->make([
+            'due_date' => date('Y-m-d H:i:s', strtotime('-1 day')),
+        ]);
+
+        $response = $this->post('tasks', $task->toArray());
+
+        $this->assertExceptionMessage($response, 'due_date must be greater than today\'s date.');
+    }
+
+    /** @test */
     public function test_update_task_fails_when_title_is_not_passed()
     {
         $task = factory(Task::class)->create();
@@ -48,6 +67,27 @@ class TaskControllerValidationTest extends TestCase
 
         $this->put('tasks/'.$task->id, ['title' => 'random string', 'project_id' => ''])
             ->assertSessionHasErrors(['project_id' => 'The project id field is required.']);
+    }
+
+    /** @test */
+    public function test_update_task_fail_with_non_existing_project_id()
+    {
+        $task = factory(Task::class)->create();
+
+        $this->put('tasks/'.$task->id, ['project_id' => 999])
+            ->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function test_update_task_fail_with_invalid_due_date()
+    {
+        $task = factory(Task::class)->create();
+        $dueDate = date('Y-m-d H:i:s', strtotime('-1 day'));
+
+        $input = array_merge($task->toArray(), ['due_date' => $dueDate]);
+        $response = $this->put('tasks/'.$task->id, $input);
+
+        $this->assertExceptionMessage($response, 'due_date must be greater than today\'s date.');
     }
 
     /** @test */
