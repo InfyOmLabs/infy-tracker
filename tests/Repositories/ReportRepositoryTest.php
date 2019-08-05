3bab621a025629b5253addc3920035e0d7282d8e
Farhan <farhanali.kathawala@infyom.com>
2019-08-05 17:09:14 +0530

    test(report): test cases for update report filter

--- tests/Repositories/ReportRepositoryTest.php
+++ tests/Repositories/ReportRepositoryTest.php
@@ -9,6 +9,7 @@ use App\Models\ReportFilter;
 use App\Models\Tag;
 use App\Models\User;
 use App\Repositories\ReportRepository;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -154,6 +155,104 @@ class ReportRepositoryTest extends TestCase
 
         $this->assertNotEmpty($duration);
         $this->assertEquals('2 hr', $duration);
+    }
+
+    /** @test */
+    public function test_can_update_report_filter_for_given_project_ids()
+    {
+        $report = factory(Report::class)->create();
+
+        /** @var Collection $projects */
+        $projects = factory(Project::class)->times(3)->create();
+
+        $this->generateReportFilter($report->id, $projects[0]->id, Project::class);
+        $this->generateReportFilter($report->id, $projects[1]->id, Project::class);
+
+        $input = [
+            'projectIds' => [$projects[0]->id, $projects[2]->id],
+        ];
+        $updatedReportFilter = $this->reportRepo->updateReportFilter($input, $report);
+
+        $this->assertNotEmpty($updatedReportFilter);
+        $this->assertEquals($projects[2]->id, $updatedReportFilter[0]->param_id);
+        $this->assertEquals(Project::class, $updatedReportFilter[0]->param_type);
+
+        $reportFilter = ReportFilter::ofParamType(Project::class)
+            ->where('param_id', $projects[1]->id)
+            ->first();
+        $this->assertEmpty($reportFilter);
+    }
+
+    /** @test */
+    public function test_can_update_report_filter_for_given_user_ids()
+    {
+        $report = factory(Report::class)->create();
+
+        /** @var Collection $users */
+        $users = factory(User::class)->times(3)->create();
+
+        $this->generateReportFilter($report->id, $users[0]->id, User::class);
+        $this->generateReportFilter($report->id, $users[1]->id, User::class);
+
+        $input = [
+            'userIds' => [$users[0]->id, $users[2]->id],
+        ];
+        $updatedReportFilter = $this->reportRepo->updateReportFilter($input, $report);
+
+        $this->assertNotEmpty($updatedReportFilter);
+        $this->assertEquals($users[2]->id, $updatedReportFilter[0]->param_id);
+        $this->assertEquals(User::class, $updatedReportFilter[0]->param_type);
+
+        $reportFilter = ReportFilter::ofParamType(User::class)
+            ->where('param_id', $users[1]->id)
+            ->first();
+        $this->assertEmpty($reportFilter);
+    }
+
+    /** @test */
+    public function test_can_update_report_filter_for_given_tag_ids()
+    {
+        $report = factory(Report::class)->create();
+
+        /** @var Collection $tags */
+        $tags = factory(Tag::class)->times(3)->create();
+
+        $this->generateReportFilter($report->id, $tags[0]->id, Tag::class);
+        $this->generateReportFilter($report->id, $tags[1]->id, Tag::class);
+
+        $input = [
+            'tagIds' => [$tags[0]->id, $tags[2]->id],
+        ];
+        $updatedReportFilter = $this->reportRepo->updateReportFilter($input, $report);
+
+        $this->assertNotEmpty($updatedReportFilter);
+        $this->assertEquals($tags[2]->id, $updatedReportFilter[0]->param_id);
+        $this->assertEquals(Tag::class, $updatedReportFilter[0]->param_type);
+
+        $reportFilter = ReportFilter::ofParamType(Tag::class)
+            ->where('param_id', $tags[1]->id)
+            ->first();
+        $this->assertEmpty($reportFilter);
+    }
+
+    /** @test */
+    public function test_can_update_report_filter_for_given_client_id()
+    {
+        $report = factory(Report::class)->create();
+
+        /** @var Collection $clients */
+        $clients = factory(Client::class)->times(2)->create();
+
+        $this->generateReportFilter($report->id, $clients[1]->id, Client::class);
+
+        $input = [
+            'client_id' => $clients[0]->id,
+        ];
+        $updatedReportFilter = $this->reportRepo->updateReportFilter($input, $report);
+
+        $this->assertNotEmpty($updatedReportFilter);
+        $this->assertEquals($clients[0]->id, $updatedReportFilter[0]->param_id);
+        $this->assertEquals(Client::class, $updatedReportFilter[0]->param_type);
     }
 
     /**
