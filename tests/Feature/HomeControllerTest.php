b1b5969269e91bd50733e0901fe1cb345310e622
Mitul Golakiya <mtl.golakiya@gmail.com>
2019-08-23 17:43:29 +0530

    test(dashboard): view data assertion added

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -2,12 +2,17 @@
 
 namespace Tests\Feature;
 
+use App\Repositories\UserRepository;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class HomeControllerTest extends TestCase
 {
     use DatabaseTransactions;
+
+    /** @var MockInterface */
+    protected $userRepository;
 
     public function setUp(): void
     {
@@ -15,13 +20,26 @@ class HomeControllerTest extends TestCase
         $this->signInWithDefaultAdminUser();
     }
 
+    private function mockRepository()
+    {
+        $this->userRepository = \Mockery::mock(UserRepository::class);
+        app()->instance(UserRepository::class, $this->userRepository);
+    }
+
     /** @test */
     public function it_shows_dashboard()
     {
+        $this->mockRepository();
+
+        $mockedResponse = [['id' => 1, 'name' => 'Dummy User']];
+        $this->userRepository->expects('getUserList')
+            ->andReturn($mockedResponse);
+
         $response = $this->get(route('home'));
 
         $response->assertStatus(200)
                  ->assertViewIs('dashboard.index')
+                 ->assertViewHas('users', $mockedResponse)
                  ->assertSeeText('Dashboard')
                  ->assertSeeText('Custom Report')
                  ->assertSeeText('Daily Work Report');
