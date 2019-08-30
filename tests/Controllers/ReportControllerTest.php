a555134f6ea028231bee6c3490ebf01e5ce4c8d6
Farhan <kathawalafarhan@gmail.com>
2019-08-30 16:42:05 +0530

    test(report): minor changes

--- tests/Controllers/ReportControllerTest.php
+++ tests/Controllers/ReportControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Controllers;
 
 use App\Models\Report;
-use App\Models\TimeEntry;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -19,14 +18,10 @@ class ReportControllerTest extends TestCase
     }
 
     /** @test */
-    public function test_can_filter_report_by_created_user()
+    public function test_reports_are_filtered_using_created_by()
     {
-        /** @var TimeEntry $firstTimeEntry */
-        $firstTimeEntry = factory(TimeEntry::class)->create();
-        $secondTimeEntry = factory(TimeEntry::class)->create();
-
         /** @var Report $firstReport */
-        $firstReport = factory(Report::class)->create(['owner_id' => $firstTimeEntry->user_id]);
+        $firstReport = factory(Report::class)->create();
         $secondReport = factory(Report::class)->create();
 
         $response = $this->getJson(route('reports.index', [
@@ -35,7 +30,7 @@ class ReportControllerTest extends TestCase
 
         $data = $response->original['data'];
         $this->assertCount(1, $data);
-        $this->assertEquals($firstTimeEntry->id, $data[0]['id']);
+        $this->assertEquals($firstReport->id, $data[0]['id']);
         $this->assertEquals($firstReport->owner_id, $data[0]['owner_id']);
     }
 }
