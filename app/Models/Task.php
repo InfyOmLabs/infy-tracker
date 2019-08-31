d7a3c28f2b9ce22ced6d8fc864edbd6459c5b5dc
Mitul Golakiya <mtl.golakiya@gmail.com>
2019-08-31 03:34:56 +0000

    Apply fixes from StyleCI

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
