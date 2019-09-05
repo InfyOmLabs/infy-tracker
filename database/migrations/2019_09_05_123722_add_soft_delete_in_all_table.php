2c945d060ff2da05d36b8756ee1a9ac4a163c31a
Monika vaghasiya <monika.vaghasiya@infyom.com>
2019-09-05 18:35:58 +0530

    fix(migration): added migration for deleted at and deleted by

---
+++ database/migrations/2019_09_05_123722_add_soft_delete_in_all_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeleteInAllTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('activity_types', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->integer('deleted_by');
+        });
+        Schema::table('clients', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->integer('deleted_by');
+        });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->integer('deleted_by');
+        });
+        Schema::table('tags', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->integer('deleted_by');
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->integer('deleted_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('activity_types', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->dropColumn('deleted_by');
+        });
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->dropColumn('deleted_by');
+        });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->dropColumn('deleted_by');
+        });
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->dropColumn('deleted_by');
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->dropColumn('deleted_by');
+        });
+    }
+}
