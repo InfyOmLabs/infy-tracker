2a627918557f85e580d374872c839cfb82b2a243
Farhan Kathawala <37834439+farhaninfyom@users.noreply.github.com>
2019-09-12 13:20:47 +0530

    test: test cases for helper (#340)

--- tests/Unit/Helpers/HelpersTest.php
+++ tests/Unit/Helpers/HelpersTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Unit\Helpers;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class HelpersTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /** @test */
     public function explode_trim_remove_empty_values_from_array()
     {
@@ -23,5 +27,42 @@ class HelpersTest extends TestCase
 
         $result = explode_trim_remove_empty_values_from_array(' hello ,, ,0, test   ');
         $this->assertEquals(['hello', '0', 'test'], $result);
+    }
+
+    /** @test */
+    public function test_return_user_avatar_url()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create();
+
+        $response = getUserImageInitial($user->id, $user->name);
+
+        $this->assertNotEmpty($response);
+        $this->assertStringContainsString($user->name, $response);
+    }
+
+    /** @test */
+    public function test_return_true_when_user_has_permission()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $this->attachPermissions($user->id, ['manage_users']);
+
+        $response = authUserHasPermission('manage_users');
+
+        $this->assertTrue($response);
+    }
+
+    /** @test */
+    public function test_return_false_when_user_has_no_permission()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = authUserHasPermission('manage_users');
+
+        $this->assertFalse($response);
     }
 }
