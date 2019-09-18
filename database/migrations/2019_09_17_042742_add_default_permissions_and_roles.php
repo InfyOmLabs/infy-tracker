5f54d35918e8302663901a86b4daba6c496c4a6e
Monika vaghasiya <monika.vaghasiya@infyom.com>
2019-09-18 12:50:42 +0530

    refactor(migration): assign permission to new permission table

--- database/migrations/2019_09_17_042742_add_default_permissions_and_roles.php
+++ database/migrations/2019_09_17_042742_add_default_permissions_and_roles.php
@@ -16,56 +16,13 @@ class AddDefaultPermissionsAndRoles extends Migration
     public function up()
     {
         //create permissions
-        $permissions = [
-            [
-                'name'         => 'manage_clients',
-                'display_name' => 'Manage Clients',
-                'description'  => 'Visible clients tab and manage it.',
-            ],
-            [
-                'name'         => 'manage_projects',
-                'display_name' => 'Manage Projects',
-                'description'  => 'Project tab visible and manage it.',
-            ],
-            [
-                'name'         => 'manage_all_tasks',
-                'display_name' => 'Manage Tasks',
-                'description'  => 'All projects list comes into Project filter otherwise comes only related projects.Assignee Filter visible in task module otherwise own assigned and non-assigned.',
-            ],
-            [
-                'name'         => 'manage_time_entries',
-                'display_name' => 'Manage Entry',
-                'description'  => 'User can manage own time entry.',
-            ],
-            [
-                'name'         => 'manage_users',
-                'display_name' => 'Manage Users',
-                'description'  => 'User tab visible',
-            ],
-            [
-                'name'         => 'manage_tags',
-                'display_name' => 'Manage Tags',
-                'description'  => 'Able to access tags tab.',
-            ],
-            [
-                'name'         => 'manage_activities',
-                'display_name' => 'Manage Activities',
-                'description'  => 'Able to access Activity tab.',
-            ],
-            [
-                'name'         => 'manage_reports',
-                'display_name' => 'Manage Reports',
-                'description'  => '',
-            ],
-            [
-                'name'         => 'manage_roles',
-                'display_name' => 'Manage Roles',
-                'description'  => '',
-            ],
-
-        ];
-        foreach ($permissions as $permission) {
-            Permission::create($permission);
+        $oldPermissions = DB::table('entrust_permissions')->get();
+        foreach ($oldPermissions as $permission) {
+            Permission::create([
+                'name'         => $permission->name,
+                'display_name' => $permission->display_name,
+                'description'  => $permission->description,
+            ]);
         }
 
         $oldRoles = DB::table('entrust_roles')->get();
