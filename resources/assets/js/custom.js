e45678e79192c3dfac197296c4f6f7ac7179133f
Monika vaghasiya <monika.vaghasiya@infyom.com>
2019-09-19 15:11:24 +0530

    fix(UI): solve colapse icon issue in time entry datatable and task detail pop up

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -253,13 +253,3 @@ window.nl2br = function (str, is_xhtml) {
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 };
-
-$(document).on('click', '.collapse-icon', function () {
-    let isShow = $(this).parent().parent().hasClass('shown');
-    if (isShow) {
-        $(this).children().removeClass('fa-plus-circle').addClass("fa-minus-circle");
-    } else {
-        $(this).children().removeClass('fa-minus-circle').addClass("fa-plus-circle");
-    }
-});
-
