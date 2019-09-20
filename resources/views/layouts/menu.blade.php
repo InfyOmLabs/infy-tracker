181ace20957790a579f255f0f3aa68360edd440e
Monika vaghasiya <monika.vaghasiya@infyom.com>
2019-09-20 11:43:05 +0530

    fix(menu): set proper order of menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -12,14 +12,6 @@
 </li>
 @endcan
 
-@can('manage_users')
-<li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
-    <a class="nav-link" href="{!! route('users.index') !!}">
-        <i class="fa fa-users nav-icon" aria-hidden="true"></i>&nbsp;&nbsp;Users
-    </a>
-</li>
-@endcan
-
 @can('manage_projects')
 <li class="nav-item {{ Request::is('projects*') ? 'active' : '' }}">
     <a class="nav-link" href="{!! route('projects.index') !!}">
@@ -47,6 +39,14 @@
         <i class="fa fa-file nav-icon" aria-hidden="true"></i>&nbsp;&nbsp;Reports
     </a>
 </li>
+@endcan
+
+@can('manage_users')
+    <li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
+        <a class="nav-link" href="{!! route('users.index') !!}">
+            <i class="fa fa-users nav-icon" aria-hidden="true"></i>&nbsp;&nbsp;Users
+        </a>
+    </li>
 @endcan
 @can('manage_roles')
 <li class="nav-item {{ Request::is('roles*') ? 'active' : '' }}">
