a3d85a9caa125e4d0955d96b11ff10a9b58df3f7
Monika vaghasiya <monika.vaghasiya@infyom.com>
2019-09-27 15:25:52 +0530

    feat(task): if stopwatch runnning than that task can not be complete until timer for that task stops.

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -253,3 +253,7 @@ window.nl2br = function (str, is_xhtml) {
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 };
+
+window.getItemFromLocalStorage = function(item) {
+    return localStorage.getItem(item+'_'+loggedInUserId);
+};
