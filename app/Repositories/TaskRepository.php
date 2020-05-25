1ebbc82d77205ec7f5969ec462ad53051d7da459
Rakesh Patel <rakesh.patel@infyom.com>
2020-05-25 15:21:16 +0530

    refactor(task): assign the task by default who created the same

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -88,8 +88,11 @@ class TaskRepository extends BaseRepository
                 $this->attachTags($task, $input['tags']);
             }
 
-            if (isset($input['assignees']) && !empty($input['assignees'])) {
+            if (isset($input['assignees']) && ! empty($input['assignees'])) {
+                array_push($input['assignees'], getLoggedInUserId());
                 $task->taskAssignee()->sync($input['assignees']);
+            } else {
+                $task->taskAssignee()->sync(getLoggedInUserId());
             }
             DB::commit();
         } catch (Exception $e) {
@@ -127,7 +130,7 @@ class TaskRepository extends BaseRepository
             $tags = !empty($input['tags']) ? $input['tags'] : [];
             $this->attachTags($task, $tags);
 
-            $assignees = !empty($input['assignees']) ? $input['assignees'] : [];
+            $assignees = ! empty($input['assignees']) ? $input['assignees'] : $input['assignees'] = getLoggedInUserId();
             $task->taskAssignee()->sync($assignees);
 
             DB::commit();
