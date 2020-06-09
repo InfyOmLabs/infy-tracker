bc5c2c0466adab3cb5e55fcd7ce2d25f8a51197d
Rakesh Patel <rakesh.patel@infyom.com>
2020-06-09 15:47:28 +0530

    enhancement(task): Implement the Quill Editor in place of CKEditor

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,23 +14,37 @@ const mix = require('laravel-mix');
 /* Copy */
 mix.copyDirectory('resources/assets/img', 'public/assets/img');
 
+mix.copy('node_modules/quill/dist/quill.snow.css',
+    'public/assets/style/css/quill.snow.css');
+
 /* CSS */
-mix
-    .sass('resources/assets/style/sass/laravel/app.scss', 'public/assets/style/css/app.css')
-    .sass('resources/assets/style/sass/style.scss', 'public/assets/style/css/style.css')
-    .sass('resources/assets/style/sass/dashboard.scss', 'public/assets/style/css/dashboard.css')
-    .sass('resources/assets/style/sass/task-detail.scss', 'public/assets/style/css/task-detail.css')
-    .sass('resources/assets/style/sass/report.scss', 'public/assets/style/css/report.css')
-    .version();
+mix.sass('resources/assets/style/sass/laravel/app.scss',
+    'public/assets/style/css/app.css').
+    sass('resources/assets/style/sass/style.scss',
+        'public/assets/style/css/style.css').
+    sass('resources/assets/style/sass/dashboard.scss',
+        'public/assets/style/css/dashboard.css').
+    sass('resources/assets/style/sass/task-detail.scss',
+        'public/assets/style/css/task-detail.css').
+    sass('resources/assets/style/sass/report.scss',
+        'public/assets/style/css/report.css').
+    version();
+
+/* Copy JS */
+mix.babel('node_modules/quill/dist/quill.min.js',
+    'public/assets/js/quill.min.js');
 
 /* JS */
-mix
-    .js('resources/assets/js/custom.js', 'public/assets/js/custom.js')
-    .js('resources/assets/js/time_tracker/time_tracker.js', 'public/assets/js/time_tracker/time_tracker.js')
-    .js('resources/assets/js/users/user.js', 'public/assets/js/users/user.js')
-    .js('resources/assets/js/time_entries/time_entry.js', 'public/assets/js/time_entries/time_entry.js')
-    .js('resources/assets/js/clients/client.js', 'public/assets/js/clients/client.js')
-    .js('resources/assets/js/projects/project.js', 'public/assets/js/projects/project.js').
+mix.js('resources/assets/js/custom.js', 'public/assets/js/custom.js').
+    js('resources/assets/js/time_tracker/time_tracker.js',
+        'public/assets/js/time_tracker/time_tracker.js').
+    js('resources/assets/js/users/user.js', 'public/assets/js/users/user.js').
+    js('resources/assets/js/time_entries/time_entry.js',
+        'public/assets/js/time_entries/time_entry.js').
+    js('resources/assets/js/clients/client.js',
+        'public/assets/js/clients/client.js').
+    js('resources/assets/js/projects/project.js',
+        'public/assets/js/projects/project.js').
     js('resources/assets/js/task/task.js', 'public/assets/js/task/task.js').
     js('resources/assets/js/activity_types/activity.js',
         'public/assets/js/activity_types/activity.js').
